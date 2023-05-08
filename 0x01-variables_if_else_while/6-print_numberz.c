790b654de2848b9865e78c9d6b51d5946994b335
juixdesigns <iloabuchi2019@gmail.com>
2023-05-08 02:28:01 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,17 +6,15 @@
 
 int main(void)
 {
-	putchar('0');
-	putchar('\n');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+
+	}
+
 	putchar('\n');
 	return (0);
 }
